4ef92e543d0e0b3b475aa084deff331d4ece7f9f
Emiliano Pintos <81244183+PerseoES@users.noreply.github.com>
2021-06-26 18:12:01 -0300

    Add welcome message

--- bottg.py
+++ bottg.py
@@ -13,6 +13,13 @@ server = Flask(__name__)
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
+    
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")    
+    
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
