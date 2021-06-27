ebf61a46338ac858131ad53fde1f7c900ca864ab
Emiliano <emilianopintos18@gmail.com>
2021-06-27 13:30:55 -0300

    Cambios

--- respuestas.py
+++ respuestas.py
@@ -9,7 +9,7 @@ def resp_simples(mensaje):
     if user_msg in ("hello", "hola", "hi", "buenas"):
         return "Hola! ¿Cómo estás?"
     
-    if user_msg in ("how are you", "cómo estás?", "como estas?", "como estas"):
+    if user_msg in ("how are you", "cómo estás?", "como estas?", "como estas", "cómo estás"):
         return "Estoy súper bien =D"
     
     if user_msg in ("who are you", "who are you?", "quién sos?", "quién eres?", "quien eres?", "quien eres", "quien sos", "quién sos?"):
@@ -17,8 +17,14 @@ def resp_simples(mensaje):
     
     if user_msg in ("time", "time?", "hora", "hora?", "qué hora es?", "dime la hora", "me puedes decir la hora?"):
         ahora = datetime.now()
-        date_time = now.strftime("%d/%m/%y, %H:%M:%S")
-        return (f"La hora es {str(date_time)}")
+        date_time = ahora.strftime("%d/%m/%y, %H:%M:%S")
+        return (f"{La hora es: str(date_time)}")
+    
+    if user_msg in ("quién es cande", "quién es candela", "quién es cande?", "quién es candela?")
+        return "Cande es una persona increíble, tendrías que conocerla si no lo has hecho aún. :D"
+    
+    if user_msg in ("quién es juan", "quién es juani", "quién es juani?", "quién es juan?")
+        return "Juani es una personita increíble en este mundo, siempre te querrá ver feliz, espero tengas la oportunidad de cruzarte con él porque créeme que jamás te arrepentirás =)."
         
     else:
-        return "No pude entender lo que me quisiste decir, por favor, reingresa otra palabra."
+        return "No pude entender lo que me quisiste decir, por favor, dime otra palabra."
