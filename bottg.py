19752f8577b1efdd943fcae1666cfb0992f8d2da
Emiliano Pintos <81244183+PerseoES@users.noreply.github.com>
2021-06-26 21:55:27 -0300

    Update bottg.py

--- bottg.py
+++ bottg.py
@@ -34,7 +34,6 @@ Haré un promedio, sólo tenes que pasarme números y finalizar el mensaje con u
 """)
 
 @bot.message_handler(commands=['instagram'])
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' is in msg.text)
 def crearlinkig(message):
     bot.reply_to(message, 'Ingresa un usuario de IG y te devolveré su link a continuación (ej. @user):')
     msj = message.text.split()
